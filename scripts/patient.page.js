a2d4a80b52dc94bee5562c6ce7f25fef04e50304
ilyabulychev <github@ilyabulychev.com>
2018-12-24 20:45:44 +0300

    QR

---
+++ scripts/patient.page.js
@@ -0,0 +1,69 @@
+var canvas = document.getElementById('rqcode');
+
+var ctx = canvas.getContext("2d");
+ctx.font = "18px Arial";
+ctx.fillText("[QR CODE WILL BE HERE]", 10, 50);
+
+
+$(document).ready(function(){ 
+
+    // GET PATIENT
+    var id = window.location.search.split('=')[1];
+
+    if(id) {
+        $.ajax( { url: "https://api.mlab.com/api/1/databases/ilyabulychev/collections/qrcodes/"+id+"?apiKey=i8dWtPvSsw7NR7H7FSbP3RXCgs1IJfBT",
+		  type: "GET",
+          contentType: "application/json",
+
+          success: function(data) {
+
+            $("#firstName").val(data.firstName);
+            $("#secondName").val(data.secondName);
+            $("#lastName").val(data.lastName);
+            $("#birthDate").val(data.birthDate);
+
+            QRCode.toCanvas(document.getElementById('rqcode'), window.location.pathname + '?id=' + data._id.$oid, function (error) {
+            if (error) console.error(error)
+                console.log('success!');
+            })
+            
+        } });
+    }
+
+    $('#save').on('click', function() {
+
+        $.ajax( { url: "https://api.mlab.com/api/1/databases/ilyabulychev/collections/qrcodes?apiKey=i8dWtPvSsw7NR7H7FSbP3RXCgs1IJfBT",
+		  data: JSON.stringify( { 
+              firstName : $("#firstName").val(),
+              secondName: $("#secondName").val(),
+              lastName: $("#lastName").val(),
+              birthDate: $("#birthDate").val()
+            } ),
+		  type: "POST",
+          contentType: "application/json",
+
+          success: function(data) {
+            alert("Patient is saved! JSON:" + JSON.stringify(data));
+
+            window.location = './patient.html?id=' +  data._id.$oid;;
+            
+        } });
+    });
+
+    
+    $('#delete').on('click', function() {
+
+        $.ajax( { url: "https://api.mlab.com/api/1/databases/ilyabulychev/collections/qrcodes/"+id+"/?apiKey=i8dWtPvSsw7NR7H7FSbP3RXCgs1IJfBT",
+        type: "DELETE",
+        contentType: "application/json",
+        
+        success: function(data) {
+            window.location = './patient.html';
+        } 
+    });
+
+    });
+});
+
+
+
---
+++ scripts/scanner.page.js
@@ -0,0 +1,83 @@
+// PATIENTS ===
+
+$(document).ready(function(){ 
+
+    $.ajax( { url: "https://api.mlab.com/api/1/databases/ilyabulychev/collections/qrcodes?apiKey=i8dWtPvSsw7NR7H7FSbP3RXCgs1IJfBT",
+        type: "GET",
+        contentType: "application/json",
+        
+        success: function(data) {
+            console.log(data);
+            data.forEach(function(patient){
+                let clone = $("#template").clone().appendTo("#patients");
+                $(clone).show();
+                $(clone).attr('id', 'patient-' + patient._id.$oid);
+                $(clone).find('.firstName').html(patient.firstName);
+                $(clone).find('.secondName').html(patient.secondName);
+                $(clone).find('.lastName').html(patient.lastName);
+                $(clone).find('.birthDate').html(patient.birthDate);
+                $(clone).find('a').attr('href', './patient.html?id=' + patient._id.$oid);
+              });
+
+        } 
+    });
+
+});
+
+
+// SCANNER LOADER ===
+
+var video = document.createElement("video");
+var canvasElement = document.getElementById("canvas");
+var canvas = canvasElement.getContext("2d");
+var loadingMessage = document.getElementById("loadingMessage");
+var outputContainer = document.getElementById("output");
+var outputMessage = document.getElementById("outputMessage");
+var outputData = document.getElementById("outputData");
+
+function drawLine(begin, end, color) {
+    canvas.beginPath();
+    canvas.moveTo(begin.x, begin.y);
+    canvas.lineTo(end.x, end.y);
+    canvas.lineWidth = 4;
+    canvas.strokeStyle = color;
+    canvas.stroke();
+}
+
+
+navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
+    video.srcObject = stream;
+    video.setAttribute("playsinline", true); 
+    video.play();
+    requestAnimationFrame(tick);
+}).catch((e) => { console.log(e); });
+
+function tick() {
+    loadingMessage.innerText = "Loading video..."
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+    loadingMessage.hidden = true;
+    canvasElement.hidden = false;
+    outputContainer.hidden = false;
+
+    canvasElement.height = video.videoHeight;
+    canvasElement.width = video.videoWidth;
+    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+    var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+    var code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: "dontInvert",
+    });
+    if (code) {
+        drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
+        drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
+        drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
+        drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
+        outputMessage.hidden = true;
+        outputData.parentElement.hidden = false;
+        outputData.innerText = code.data;
+    } else {
+        outputMessage.hidden = false;
+        outputData.parentElement.hidden = true;
+    }
+    }
+    requestAnimationFrame(tick);
+}
